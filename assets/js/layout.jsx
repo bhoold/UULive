36089a3e94ac9dc0c666f758a8b72fd5bf3def7e
Raven <bhoold@yahoo.com>
2020-01-21 01:48:35 +0800

    [update] 首页模板修改

--- assets/js/layout.jsx
+++ assets/js/layout.jsx
@@ -11,10 +11,11 @@
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+//console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 //let $ = require('jquery');
 //let greet = require('./greet');
+/*
 import $ from 'jquery';
 import greet from './reacttest';
 
@@ -23,3 +24,5 @@ import greet from './reacttest';
 $(document).ready(function() {
     //$('body').prepend('<h1>'+greet('symfony')+'</h1>');
 });
+*/
+
